ef2e00f28e80fda273deaad6b5417e06f3527d94
Dhanya Lakshmi s s <dhanyalakshmi.s.s.06@gmail.com>
2025-02-19 10:37:02 +0530

    Completed

---
+++ react-1/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <h1 className='home'> HomePage</h1><hr></hr>
+      <div className='main'><h4 className='name'>Name:Dhanya Lakshmi S S</h4>
+      <h4 className='email'>Email:dhanya@gmail.com</h4>
+      <h4 className='ph'>Phone No:8610339182</h4>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
